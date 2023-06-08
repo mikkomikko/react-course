9787f4f5de4ebb7f719aafb7e629d1a64ec2932e
Mikko Suominen <mikko.suominen_external@wartsila.com>
2023-06-08 13:15:39 +0300

    Helper function for languages

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,24 @@ function App() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ul className="bg-slate-50 p-4 grid grid-cols-4 gap-4 text-sm leading-6">
-        <CountryListItem name="Finland" capital="Helsinki" region="Europe" />
-        <CountryListItem name="Sweden" capital="Stockholm" region="Europe" />
-        <CountryListItem name="Norway" capital="Oslo" region="Europe" />
+        <CountryListItem
+          name="Finland"
+          capital="Helsinki"
+          region="Europe"
+          languages={["Finnish", "Swedish"]}
+        />
+        <CountryListItem
+          name="Sweden"
+          capital="Stockholm"
+          region="Europe"
+          languages={["Swedish"]}
+        />
+        <CountryListItem
+          name="Norway"
+          capital="Oslo"
+          region="Europe"
+          languages={["Nynorsk", "Bokmål"]}
+        />
       </ul>
     </div>
   );
--- src/components/CountryListItem/CountryListItem.tsx
+++ src/components/CountryListItem/CountryListItem.tsx
@@ -2,17 +2,22 @@ interface Props {
   name: string;
   region: string;
   capital: string;
-  languages?: string[];
+  languages: string[];
 }
 
 export default function CountryListItem(props: Props) {
   const { name, region, capital, languages } = props;
+
+  const renderLanguages = () => {
+    return languages.join(", ");
+  };
 
   return (
     <li className="hover:ring-blue-500 hover:shadow-md group rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm">
       <h2 className="text-xl font-semibold">{name}</h2>
       <p className="text-slate-500">Region: {region}</p>
       <p className="text-slate-500">Capital: {capital}</p>
+      <p className="text-slate-500">Official languages: {renderLanguages()}</p>
     </li>
   );
 }
