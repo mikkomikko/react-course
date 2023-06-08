354fd965f2bf483d16b98636aaa1a1380832d015
Mikko Suominen <mikko.suominen_external@wartsila.com>
2023-06-08 15:03:37 +0300

    useState with text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Counter from "./components/Counter/Counter";
 import CountryListItem from "./components/CountryListItem/CountryListItem";
 
 function App() {
   const [currentCount, setCurrentCount] = useState<number>(0);
+  const [currentText, setCurrentText] = useState<string>("");
 
   const handleCountClick = () => {
     // setCurrentCount(currentCount + 1);
     setCurrentCount((prev) => prev + 1);
   };
 
+  const handleTextInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCurrentText(event.target.value);
+  };
+
   return (
-    <div>
-      <ul className="bg-slate-50 p-4 grid grid-cols-4 gap-4 text-sm leading-6">
+    <div className="max-w-7xl mx-auto">
+      <ul className="p-4 grid grid-cols-4 gap-4 text-sm leading-6">
         <CountryListItem
           name="Finland"
           capital="Helsinki"
@@ -32,7 +37,7 @@ function App() {
           languages={["Nynorsk", "Bokmål"]}
         />
       </ul>
-      <div>
+      <div className="mb-4">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleCountClick}
@@ -40,6 +45,22 @@ function App() {
           Add count
         </button>
         <Counter count={currentCount} />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+          id="new-password"
+          type="text"
+          onChange={handleTextInputChange}
+          value={currentText}
+        />
+        <p className="mt-3">{currentText}</p>
       </div>
     </div>
   );
