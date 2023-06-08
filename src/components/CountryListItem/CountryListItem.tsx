5c3e5ce75728074dd68a3ed9fda305b798ac87f2
Mikko Suominen <mikko.suominen_external@wartsila.com>
2023-06-08 11:46:05 +0300

    Props interface

--- src/components/CountryListItem/CountryListItem.tsx
+++ src/components/CountryListItem/CountryListItem.tsx
@@ -1,12 +1,13 @@
-export default function CountryListItem({
-  name,
-  region,
-  capital,
-}: {
+interface Props {
   name: string;
   region: string;
   capital: string;
-}) {
+  languages?: string[];
+}
+
+export default function CountryListItem(props: Props) {
+  const { name, region, capital, languages } = props;
+
   return (
     <li className="hover:ring-blue-500 hover:shadow-md group rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm">
       <h2 className="text-xl font-semibold">{name}</h2>
