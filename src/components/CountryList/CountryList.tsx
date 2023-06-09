98fac75964e0392490830ce85fc394aa0e25e620
Mikko Suominen <mikko.suominen_external@wartsila.com>
2023-06-09 15:20:53 +0300

    Filter by country name

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import CountryListItem from "../CountryListItem/CountryListItem";
 import TextInput from "../TextInput/TextInput";
 import { Country } from "../../types/types";
@@ -9,20 +9,33 @@ export default function CountryList() {
   const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
-    console.log("effect");
     axios
       .get(
         "https://restcountries.com/v3.1/all?fields=cca3,name,flags,capital,currencies,region,languages,capitalInfo,landlocked"
       )
       .then((response) => {
-        console.log("promise fulfilled");
         setCountries(response.data);
       })
       .catch((e) => {
         console.log(e);
       });
   }, []);
-  console.log("render", countries.length, "countries");
+
+  const filteredCountries = useMemo(() => {
+    if (!countries.length) {
+      return [];
+    }
+
+    if (!filterValue.trim()) {
+      return countries;
+    }
+
+    return countries
+      .filter((country) =>
+        country.name.common.toLowerCase().includes(filterValue.toLowerCase())
+      )
+      .sort((a, b) => a.name.common.localeCompare(b.name.common));
+  }, [countries, filterValue]);
 
   const handleFilterChange = (value: string) => {
     setFilterValue(value);
@@ -32,7 +45,7 @@ export default function CountryList() {
     <div>
       <TextInput onChange={handleFilterChange} />
       <ul className="p-4 grid grid-cols-4 gap-4 text-sm leading-6">
-        {countries.map((country) => {
+        {filteredCountries.map((country) => {
           return <CountryListItem key={country.cca3} country={country} />;
         })}
       </ul>
